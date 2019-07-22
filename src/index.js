800b22dd69c2aeba36cd96ef11250ec6187f21db
Rafael Ahrons <rafael.ahrons@gmail.com>
2019-07-21 22:39:34 -0300

    Add get request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
@@ -13,23 +14,52 @@ class App extends React.Component {
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleChapterChange = this.handleChapterChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        
+
     }
 
     handleNameChange(event) {
         this.setState({ name: event.target.value });
     }
 
-    handleChapterChange(event){
-        this.setState({chapter: event.target.value});
+    handleChapterChange(event) {
+        this.setState({ chapter: event.target.value });
     }
 
     handleSubmit(event) {
         console.log('Name submited = ' + this.state.name);
         console.log('Chapter = ' + this.state.chapter);
+        debugger;
+        axios.post('https://localhost:44348/api/manhua/add',
+            {
+                params: {
+                    name: this.state.name,
+                    chapter: this.state.chapter
+                }
+            }, {
+                headers: {
+                    //'Access-Control-Allow-Origin': 'https://localhost:3000/',
+                    //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                console.log(response.data);
+            }
+        );        
         event.preventDefault();
     }
-
+    
+    getAllManhua(){
+        axios.get('https://localhost:44348/api/manhua/',{
+                headers: {
+                    //'Access-Control-Allow-Origin': 'https://localhost:3000/',
+                    //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                console.log(response.data);
+            }
+        );
+    }
     render() {
         return (
             <div>
@@ -38,7 +68,7 @@ class App extends React.Component {
                         <input type="text" value={this.state.name} onChange={this.handleNameChange} />
                     </label>
                     <label>
-                        Chapter: 
+                        Chapter:
                         <input type="number" value={this.state.chapter} onChange={this.handleChapterChange} />
                     </label>
                     <input type="submit" value="Save" />
