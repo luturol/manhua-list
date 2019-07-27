eb168cb16bf6f9f4b3c5cf99d44443e2f2c63c29
Rafael Ahrons <rafael.ahrons@gmail.com>
2019-07-27 19:59:43 -0300

    Add manhua grid

---
+++ src/GridManhua.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+
+class GridManhua extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            manhuas: []
+        }; 
+        this.getAllManhua();
+    }
+
+    getAllManhua() {
+        axios.get('https://localhost:44348/api/manhua/', {
+            headers: {                
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            console.log(response.data);
+           this.setState({manhuas: response.data});
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render() {        
+        return (
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Chapter</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.manhuas.length > 0 ? this.state.manhuas.map(manhua => <tr key={manhua.id}><td>{manhua.name}</td><td>{manhua.chapter}</td></tr>) : null
+                        }    
+                    </tbody>
+                </table>                
+            </div>
+        );
+    }
+}
+
+export default GridManhua
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
+import GridManhua from './GridManhua';
 
 class App extends React.Component {
     constructor(props) {
@@ -14,7 +15,6 @@ class App extends React.Component {
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleChapterChange = this.handleChapterChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
     }
 
     handleNameChange(event) {
@@ -26,40 +26,21 @@ class App extends React.Component {
     }
 
     handleSubmit(event) {
-        console.log('Name submited = ' + this.state.name);
-        console.log('Chapter = ' + this.state.chapter);
-        debugger;
         axios.post('https://localhost:44348/api/manhua/add',
+            this.state,
             {
-                params: {
-                    name: this.state.name,
-                    chapter: this.state.chapter
-                }
-            }, {
                 headers: {
-                    //'Access-Control-Allow-Origin': 'https://localhost:3000/',
-                    //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
                     'Content-Type': 'application/json'
                 }
             }).then((response) => {
                 console.log(response.data);
-            }
-        );        
+            }).catch((error) => {
+                console.log(error);
+            });
+
         event.preventDefault();
     }
-    
-    getAllManhua(){
-        axios.get('https://localhost:44348/api/manhua/',{
-                headers: {
-                    //'Access-Control-Allow-Origin': 'https://localhost:3000/',
-                    //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-                    'Content-Type': 'application/json'
-                }
-            }).then((response) => {
-                console.log(response.data);
-            }
-        );
-    }
+
     render() {
         return (
             <div>
@@ -72,7 +53,8 @@ class App extends React.Component {
                         <input type="number" value={this.state.chapter} onChange={this.handleChapterChange} />
                     </label>
                     <input type="submit" value="Save" />
-                </form>
+                </form>                                
+                <GridManhua />
             </div>
         );
     }
