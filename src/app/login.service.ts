616cdbbb9b015a9e8e5a0af1e37acfb34c635691
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-03-17 11:20:30 -0300

    Add redirection, message error and success for sign up

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -10,8 +10,7 @@ export class LoginService {
 
   constructor(private http: HttpClient) { }
 
-  public login(username: string, password: string): Observable<any>{
-    debugger;
+  public login(username: string, password: string): Observable<any>{    
     let url = environment.apiEndpoint + 'login';
     let header = new HttpHeaders({'Content-Type': 'application/json'});    
     let body = {
@@ -19,5 +18,17 @@ export class LoginService {
       password: password
     };
     return this.http.post(url, body, { headers: header } );
+  }
+
+  public addUser(username: string, password: string, email: string): Observable<any> {
+    let url = environment.apiEndpoint + 'adduser';
+    let header = new HttpHeaders({'Content-Type': 'application/json'});    
+    let body = {
+      username: username,
+      email: email,
+      password: password
+    };
+    
+    return this.http.post(url, body, { headers: header });
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { LoginService } from '../login.service';
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationComponent } from '../notification/notification.component';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -16,12 +17,12 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   email = new FormControl(null, [Validators.required, Validators.email]);
   password = new FormControl(null, [Validators.required]);
-  
+
   signForm: FormGroup;
   signEmail = new FormControl(null, [Validators.required, Validators.email]);
   signPassword = new FormControl(null, [Validators.required]);
   signUser = new FormControl(null, [Validators.required]);
-  
+
   constructor(formBuilder: FormBuilder, private loginService: LoginService,
     private dialog: MatDialog, private router: Router) {
     this.loginForm = formBuilder.group({
@@ -32,7 +33,7 @@ export class LoginComponent implements OnInit {
     this.signForm = formBuilder.group({
       signEmail: this.signEmail,
       signPassword: this.signPassword,
-      signUser: this.signUser    
+      signUser: this.signUser
     });
   }
 
@@ -49,28 +50,52 @@ export class LoginComponent implements OnInit {
 
     if (username && password) {
       this.loginService.login(username, password).subscribe(res => {
-        debugger;
         if (res.token && res.msg) {
           localStorage.setItem('token', res.token);
-          this.openDialog(res.msg);
+          this.openDialog({ message: res.msg, title: 'Welcome to My Manhua List!' }).subscribe(res => {
+            this.router.navigate(['/home']);
+          });
         }
         console.log(res);
       },
         err => {
-          debugger;
-          console.log(err);
+          this.openDialog({ message: err.error.msg, title: "Sorry, but an error has occured." }).subscribe(res => {
+          });
         });
     }
   }
 
-  private openDialog(message: string): void {
+  private openDialog(data: any): Observable<any> {
     const dialogRef = this.dialog.open(NotificationComponent, {
       width: '460px',
-      data: { message: message }
+      data: data
     });
 
-    dialogRef.afterClosed().subscribe(res => {
-      this.router.navigate(['/home'])
-    });
+    return dialogRef.afterClosed();
+  }
+
+  public onSignUp(): void {
+    debugger;
+    let username = this.signForm.controls.signUser.value;
+    let email = this.signForm.controls.signEmail.value;
+    let password = this.signForm.controls.signPassword.value;
+
+    this.loginService.addUser(username, password, email).subscribe(res => {
+      if (res.msg) {
+        this.openDialog({ message: res.msg, title: "We're glad to have you here." }).subscribe(res => {
+          this.loginService.login(username, password).subscribe(res => {
+            this.router.navigate(['/home']);
+          })
+
+        });
+
+      }
+    },
+      err => {
+        debugger;
+        if (err.error) {
+          this.openDialog({ message: err.error.msg, title: "Sorry, an error has occurred :(", error: true }).subscribe(res => { });
+        }
+      });
   }
 }
